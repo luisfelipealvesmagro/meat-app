60e46278b5c7193086ce06d5e8a532892c56ea1b
Luis Felipe Alves <felipe_alvess@hotmail.com>
2018-10-28 11:26:12 -0300

    change readme

---
+++ src/app/app.error-handler.ts
@@ -0,0 +1,36 @@
+import { HttpErrorResponse } from '@angular/common/http'
+import { ErrorHandler, Injectable, Injector, NgZone } from '@angular/core'
+import { NotificationService } from './shared/messages/notification.service';
+import { LoginService } from './security/login/login.service';
+
+@Injectable()
+export class ApplicationErrorHandler extends ErrorHandler {
+
+  constructor(private ns: NotificationService, 
+              private injector: Injector,
+              private zone: NgZone) {
+    super()
+  }
+
+  handleError(errorResponse: HttpErrorResponse | any){
+    if (errorResponse instanceof HttpErrorResponse) {
+      const message = errorResponse.error.message
+      this.zone.run(() =>{
+        switch(errorResponse.status){
+          case 401:
+          this.injector.get(LoginService).handleLogin()
+          break;
+          case 403:
+            this.ns.notify(message || 'Não Autorizado - 403')
+          break;
+          case 404:
+            this.ns.notify(message || 'Recurso não encontrado - 404')
+          break;
+        }
+      })
+      
+    }
+    super.handleError(errorResponse)
+  }
+
+}
